c0ec4be87a66c72b13be6789b3b8e9395cf7de6d
Rakib31 <30778508+Rakib31@users.noreply.github.com>
2019-12-30 02:58:07 +0600

    simple realtime user access checking

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '/../public');
-app.use(express.static(publicPath));
+
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+let server = http.createServer(app);
+let io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('new user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user was disconnected');
+    })
+})
+
+server.listen(port, () => {
     console.log(`Server running on  port ${port}`);
 })
